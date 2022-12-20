3e1c65a23c1ee4733d4a2972dd89c16f3ac4b12a
Paco Castro <paco.castro@shopify.com>
2022-12-20 20:47:06 +0100

    Day 20 working version

--- day20.rb
+++ day20.rb
@@ -1,217 +1,103 @@
 module Advent2022
-  class LinkedList
-    class Node
-      attr_accessor :next, :prev
-      attr_reader :value
-
-      def initialize(value)
-        @value = value
-        @next  = nil
-        @prev = nil
-      end
-
-      def to_s
-        @value
-      end
-
-      #def ==(other)
-      #end
-    end
-
-    attr_reader :head, :tail
-
-    def initialize
-      @head = nil
-      @tail = nil
-    end
-
-    def append(value)
-      node = LinkedList::Node.new(value)
-      if @tail && @head
-        node.next = @head
-        node.prev = @tail
-
-        @head.prev = node
-        @tail.next = node
-      else
-        node.next = node
-        node.prev = node
-        @head = node
-      end
-      @tail = node
-
-      node
-    end
-    def move_node_after(origin, dest)
-      return if dest.next.equal?(origin)
-
-      before = origin.prev
-      after = origin.next
-
-      before.next = after
-      after.prev = before
-
-      new_after = dest.next
-      dest.next = origin
-      origin.prev = dest
-      new_after.prev = origin
-      origin.next = new_after
-      
-      if tail?(origin)
-        @tail = before
-      end
-      if head?(origin)
-        @head = after
-      end
-
-      if tail?(dest)
-        @head = origin
-      end
-    end
-
-    def move_node_before(origin, dest)
-      return if dest.prev.equal?(origin)
-
-      before = origin.prev
-      after = origin.next
-
-      before.next = after
-      after.prev = before
-
-      new_before = dest.prev
-      dest.prev = origin
-      origin.next = dest
-      new_before.next = origin
-      origin.prev = new_before
-
-      if tail?(origin)
-        @tail = before
-      end
-      if head?(origin)
-        @head = after
-      end
-
-      if head?(dest)
-        @tail = origin
-      end
-    end
-
-    def to_a
-      current = @head 
-      seq = []
-      loop do
-        seq << current.value
-        current = current.next
-
-        break if current.equal?(@head)
-      end
-
-      seq
-    end
-
-    def tail?(node)
-      node.equal?(@tail)
-    end
-
-    def head?(node)
-      node.equal?(@head)
-    end
-  end
 
   class GrovePositioningSystem
     attr_reader :list, :initial_sequence
 
+    DECRYPTION_KEY = 811589153
+
     def initialize
-      @list = LinkedList.new
+      @list = []
       @initial_sequence = []
       @pivot = 0
+      @append = true
     end
 
     def add_number(n)
-      node = @list.append(n)
-      @initial_sequence << node
+      @initial_sequence << @list.size
+      @list << n
+    end
 
-      node
+    def set_unshift
+      @append = false
+    end
+
+    def find_pivot(i)
+      @initial_sequence.each_with_index do |order, pos_i|
+        if order == i
+          return pos_i
+        end
+      end
+
+      -1
     end
 
     def move(i)
-      elm = @initial_sequence[i]
+      index = find_pivot(i)
 
-      dest = elm
-      if elm.value > 0
-        elm.value.times { dest = dest.next }
-        @list.move_node_after(elm, dest)
-      elsif elm.value < 0
-        elm.value.abs.times { dest = dest.prev }
-        @list.move_node_before(elm, dest)
+      value = @list[index]
+      return value if value == 0
+
+      order = @initial_sequence[index]
+      move_to = (index + value) % (@list.size-1)
+      return value if move_to == index
+
+      order = @initial_sequence.delete_at(index)
+      value = @list.delete_at(index)
+
+      if move_to == 0
+        if @append
+          @initial_sequence << order
+          @list << value
+        else
+          @initial_sequence.unshift(order)
+          @list.unshift(value)
+        end
+      else
+        @initial_sequence.insert(move_to, order)
+        @list.insert(move_to, value)
       end
 
-      elm
+      value
     end
 
     def pivot
       result = move(@pivot)
       @pivot += 1
 
-      result.value
+      result
     end
 
     def sequence
-      @list.to_a
+      @list
     end
 
     def mix
+      @pivot = 0
       @initial_sequence.each do |_|
         pivot
       end
     end
 
     def find(value)
-      current = @list.head 
-      until current.value.equal?(value) do
-        return nil if @list.tail?(current)
-        current = current.next
+      @list.each_with_index do |v, i|
+        return i if v == value
       end
 
-      current
+      nil
     end
-
-    def traverse(n, from)
-      current = from
-      n.times { current = current.next }
-
-      current
-    end
-
-=begin
-    def grove_coords2
-      mix 
-      res = @list.to_a
-      i = 0
-      while true do
-        break if res[i] == 0
-        i+=1
-      end
-      puts "0 found at #{i}"
-      puts res.size
-      puts res[(i+1000) % res.size]
-      puts res[(i+2000) % res.size]
-      puts res[(i+3000) % res.size]
-
-      [res[(i+1000) % res.size], res[(i+2000) % res.size], res[(i+3000) % res.size]]
-    end
-=end
 
     def grove_coords
-      mix
-      node0 = find(0)
+      i = find(0)
+      size = sequence.size
 
-      first = traverse(1000, node0)
-      second = traverse(1000, first)
-      third = traverse(1000, second)
-      
-      [first.value, second.value, third.value]
+      [sequence[(i+1000) % size], sequence[(i+2000) % size], sequence[(i+3000) % size]]
     end
+
+    def apply_key
+      0.upto(@list.size-1) do |i|
+        @list[i] *= DECRYPTION_KEY
+      end
+    end 
 
     def self.from(data)
       data = File.read(data) if File.exist?(data)
@@ -226,13 +112,28 @@ module Advent2022
     end
   end
 
+  # The linked list solution would have not escaled for part 2. I still don't understand why it doesn't work
+  # (that code is in the previous commit)
+  # set_unshift was an attempt to make test_rounds work
+  # IT IS NOT NECESSARY because grove_coords IS RELATIVE to the position of 0
+  # The test should be fixable by rotating everything so that 0 is at position 0
+  #
+  # It was hard for me to realize that the shift worked without left/right tricks if you removed the element first
   class Day20
     def self.run(argv)
       o = GrovePositioningSystem.from(argv[0])
      
+      o.mix
       coords = o.grove_coords
       puts coords.to_s
       puts "Part 1: #{coords.sum}"
+
+      o = GrovePositioningSystem.from(argv[0])
+      #o.set_unshift
+      o.apply_key
+      10.times { o.mix }
+      coords = o.grove_coords
+      puts "Part 2: #{coords.sum}"
     end
   end
 end
--- day20_test.rb
+++ day20_test.rb
@@ -4,27 +4,6 @@ require_relative 'day20'
 module Advent2022
   class GrovePositioningSystemTest < Minitest::Test
 
-    def test_linked_list
-      ll = LinkedList.new
-      [1, 2, 3].each { |i| ll.append(i) }
-
-      assert_equal ll.tail, ll.head.prev
-      assert_equal ll.head, ll.tail.next
-
-      center = ll.head.next
-      assert_equal 2, center.value
-      assert center.prev == ll.head
-      assert center.next == ll.tail
-
-      ll.move_node_before(center, ll.head)
-      assert_equal [1, 3, 2], ll.to_a
-
-      center = ll.head.next
-      assert_equal 3, center.value
-      ll.move_node_after(center, ll.tail)
-      assert_equal [3, 1, 2], ll.to_a
-    end
-
     def test_move_right
       gps = GrovePositioningSystem.new
       [4, 5, 6, 1, 7, 8, 9].each { |i| gps.add_number(i) }
@@ -54,44 +33,30 @@ module Advent2022
       moved = gps.pivot
       assert_equal 1, moved
       assert_equal [2, 1, -3, 3, -2, 0, 4], gps.sequence
-      assert_equal 2, gps.list.head.value
-      assert_equal 4, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal 2, moved
       assert_equal [1, -3, 2, 3, -2, 0, 4], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal 4, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal -3, moved
       assert_equal [1, 2, 3, -2, -3, 0, 4], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal 4, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal 3, moved
       assert_equal [1, 2, -2, -3, 0, 3, 4], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal 4, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal -2, moved
       assert_equal [1, 2, -3, 0, 3, 4, -2], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal -2, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal 0, moved
       assert_equal [1, 2, -3, 0, 3, 4, -2], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal -2, gps.list.tail.value
 
       moved = gps.pivot
       assert_equal 4, moved
       assert_equal [1, 2, -3, 4, 0, 3, -2], gps.sequence
-      assert_equal 1, gps.list.head.value
-      assert_equal -2, gps.list.tail.value
     end
 
     def test_mix
@@ -105,47 +70,67 @@ module Advent2022
     def test_find
       gps = GrovePositioningSystem.from(sample)
 
-      node = gps.find(0)
-      assert !node.nil?
-      assert_equal -2, node.prev.value     
-      assert_equal 4, node.next.value     
+      pos = gps.find(0)
+      assert_equal 5, pos
     end
 
     def test_dont_find
       gps = GrovePositioningSystem.from(sample)
 
-      node = gps.find(23)
-      assert node.nil?
+      pos = gps.find(23)
+      assert pos.nil?
     end
 
-
-    def test_traverse
-      gps = GrovePositioningSystem.from(sample)
-    
-      gps.mix
-
-      node0 = gps.find(0)
-      assert_equal 4, node0.prev.value     
-      assert_equal 3, node0.next.value     
-
-      first = gps.traverse(1000, node0)
-      assert_equal 4, first.value
-
-      second = gps.traverse(2000, node0)
-      assert_equal -3, second.value
-
-      third = gps.traverse(3000, node0)
-      assert_equal 2, third.value
-
-      assert_equal second.value, gps.traverse(1000, first).value
-      assert_equal third.value, gps.traverse(1000, second).value
-    end
 
     def test_grove_coords
       gps = GrovePositioningSystem.from(sample)
-    
+      gps.mix 
       coords = gps.grove_coords
+
       assert_equal [4, -3, 2], coords
+    end
+
+    def test_apply_key
+      gps = GrovePositioningSystem.from(sample)
+      gps.apply_key
+
+      assert_equal [811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612], gps.sequence
+    end
+
+    def test_rounds
+      gps = GrovePositioningSystem.from(sample)
+      gps.set_unshift
+      gps.apply_key
+
+      gps.mix
+      assert_equal [0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153], gps.sequence
+
+      gps.mix
+      assert_equal [0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153], gps.sequence
+
+      gps.mix
+      assert_equal [0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459], gps.sequence
+
+      gps.mix
+      assert_equal [0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306], gps.sequence
+
+      gps.mix
+      assert_equal [0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459], gps.sequence
+
+      gps.mix
+      assert_equal [0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459], gps.sequence
+
+      gps.mix
+      assert_equal [0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612], gps.sequence
+
+      gps.mix
+      assert_equal [0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306], gps.sequence
+
+      gps.mix
+      assert_equal [0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306], gps.sequence
+
+      gps.mix
+      assert_equal [0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153], gps.sequence
     end
 
 
