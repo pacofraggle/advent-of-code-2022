c6c2e1a8d40d5ab29acc8a3310d0d656db0fbfe2
Paco Castro <paco.castro@shopify.com>
2022-12-27 22:14:27 +0100

    Day 16 Part 1
    Works but for the input-file I think it's too slow.
    I should revisit the trimming condition.
    Without this condition the algorithm doesn't finish.
    
    Maybe change the approach to no recursive to remove enqueued
    ones that can be discarded?
    Or maybe changing to BFS?
    Also clean the output code a little and store all maximal paths
    
    Test:     1.6416219999082386 sec
    Ssmple: 752.7605870002881 sec.

--- day16.rb
+++ day16.rb
@@ -1,4 +1,4 @@
-require 'set'
+require 'pry'
 
 module Advent2022
 
@@ -6,78 +6,294 @@ module Advent2022
 
   class Graph
     def initialize(label_method, neighbours_method)
-      @label_method = label
-      @neighbours_method = neigbhours
+      @label_method = label_method
+      @neighbours_method = neighbours_method
+      @graph = nil
+      @labels = nil
+      @vertices = nil
     end
     
     def build(objects)
       @graph = Array.new(objects.size)
+      @vertices = Array.new(objects.size)
       0.upto(@graph.size-1) { |i| @graph[i] = Array.new(@graph.size, 0) }
 
       @labels = {}
-      objects.each_with_index { |obj, i| @labels[label(obj)] = i }
+      objects.each_with_index do |obj, i|
+        @labels[find_label(obj)] = i
+        @vertices[i] = obj
+      end
+
       objects.each_with_index do |obj, i| 
-        p = pos(label(obj))
-        neighbours(obj).each do |n|
+        p = pos(find_label(obj))
+        find_neighbours(obj).each do |n|
           pn = pos(n)
           @graph[p][pn] = 1
         end
       end
     end
 
+    def vertices
+      raise 'Not built yet' unless @vertices
+
+      @labels.keys
+    end
+
+    def vertex(name)
+      raise 'Not built yet' unless @vertices
+
+      @vertices[@labels[name]]
+    end
+
+    def neighbours_names(vertex)
+      raise 'Not built yet' unless @vertices
+
+      names = []
+      @graph[@labels[vertex]].each_with_index do |v, i|
+        next if v == 0
+        names << @labels.key(i)
+      end
+
+      names
+    end
+
+    def neighbours(vertex)
+      neighbours_names(vertex).map { |n| vertex(n) }
+    end
+
+    def print
+      w = @labels.keys.map { |l| l.size }.max + 1
+      puts " ".ljust(w)+"|"+@labels.keys.map { |l| l.rjust(w) }.join("|")
+      sep = (@labels.size+1).times.map { "-".ljust(w, "-") }.join("+")
+      @labels.each do |l, i|
+        puts sep
+        puts l.rjust(w)+"|"+@graph[i].map { |gi| gi.to_s.rjust(w) }.join("|")
+      end
+    end
+
     private
 
-    def label(obj)
+    def find_label(obj)
       obj.send(@label_method)
     end
 
-    def neighbours(obj)
-      obj.send(@neighbours)
+    def find_neighbours(obj)
+      obj.send(@neighbours_method)
     end
-
 
     def pos(label)
       @labels[label]
     end
+  end
 
+  class CavePath
+    attr_reader :visited, :open
+    MAX = 30
+    def initialize(root, valves, open=nil, pressures=nil)
+      @visited = []
+      @visited << root
+      @visited.flatten!
+
+      if open.nil?
+        @open = {}
+        valves.select { |v| v.flow_rate > 0 && !v.open }.each { |v| @open[v.name] = nil }
+      else
+        @open = open
+      end
+
+      if pressures.nil?
+        @pressures = {}
+        valves.select { |v| v.flow_rate > 0 && !v.open }.each { |v| @pressures[v.name] = v.flow_rate }
+      else
+        @pressures = pressures
+      end
+    end
+
+    def alternative
+      CavePath.new(@visited.clone, nil, @open.clone, @pressures.clone)
+    end
+
+    def finished?
+      @visited.size == MAX || @open.find { |_, v| v.nil? }.nil?
+    end
+
+    def visit(valve)
+      raise 'Already finished' if finished?
+
+      @visited << valve
+    end
+
+    def time
+      @visited.size-1
+    end
+
+    def valve
+      @visited.last.upcase
+    end
+
+    def openable?
+      @open.key?(@visited.last) && @open[@visited.last].nil?
+    end
+
+    def open
+      return false unless openable?
+
+      @visited << @visited.last.downcase
+      @open[@visited.last.upcase] = time
+
+      true
+    end
+
+    def pressure_release(upto=MAX)
+      total = 0
+      @open.select { |_, t| !t.nil? && t < upto }.each do |v, t|
+        total += (upto - t)*@pressures[v]
+      end
+
+      total
+    end
+
+    def potential_max_release
+      final_pressure = pressure_release
+      potential_openings = (MAX-time) / 2
+      return pressure_release if potential_openings == 0
+
+      available_valves = @open.select { |k, v| v.nil? }.keys
+
+      return pressure_release if available_valves.empty?
+
+      available_pressure = @pressures.select { |k, v| available_valves.include?(k) }.sort_by { |_, v| v }.reverse.to_a
+      0.upto([potential_openings, available_pressure.size].min-1) do |i|
+        pressure = available_pressure[i][1]
+        from = time + (i+1)#*2
+        final_pressure += (MAX-from)*pressure
+      end
+
+      final_pressure
+    end
+
+    #def ==(other)
+    #  @visited == other.visited && @open == other.open
+    #end
+
+    def to_s
+      @visited.join("")
+    end
   end
 
   class ProboscideaVolcanium
   
+    attr_reader :graph, :valves
 
     def initialize(valves)
       @graph = Graph.new(:name, :tunnels)
       @graph.build(valves)
-    end
-
-    def add_valve(valve)
-      @valves[valve.name] = valve
+      @valves = valves
     end
 
     def self.from(data)
       data = File.read(data) if File.exist?(data)
 
       valves = []
-
       data.each_line do |line|
         l = line.strip
         words = l.split(/ /)
         name = words[1]
         flow = words[4].split(/=/)[1].gsub(/;/, '').to_i
-        tunnels[name] = words[9..-1].map { |v| v.gsub(/,/, '') }
+        tunnels = words[9..-1].map { |v| v.gsub(/,/, '') }
         
-        valves << Valve.new(words[1], flow, tunnels, false)
+        valves << Valve.new(name, flow, tunnels, false)
       end
 
       ProboscideaVolcanium.new(valves)
     end
 
+    def record_max(path)
+      suggested = path.pressure_release
+      if suggested > @max
+        @max = suggested
+        @max_path = [path]
+        puts "New #{@max}: #{@max_path.first}"
+        return true
+      end
+
+      if suggested == @max
+        @max_path << path
+        #puts "New #{@max}: #{@max_path.last}"
+        return true
+      end
+
+      false
+    end
+
+    def valid?(candidate)
+      candidate.potential_max_release >= @max
+    end
+
+    def max_pressure(path)
+      if path.finished?
+        record_max(path)
+        return
+      end
+
+      return unless valid?(path)
+
+      candidates = []
+      if path.openable?
+        alt = path.alternative
+        alt.open
+        candidates << alt
+      end
+      current = path.valve
+      @graph.neighbours_names(current).each do |neigh|
+        alt = path.alternative
+        alt.visit neigh
+        candidates << alt if valid?(alt)
+      end
+
+      if candidates.empty?
+        record_max(path)
+        return
+      end
+
+      #candidates.sort_by! { |alt| @graph.vertex(alt.valve).flow_rate }
+
+      candidates_pressures = []
+      candidates.each do |path|
+        #if path.finished?
+        #  candidates_pressures << path.pressure_release
+        #else
+        max_pressure(path)
+        #  options_pressures << max_pressure(path)
+        #end
+      end
+
+      #max = options_pressures.max
+      #index = options_pressures.index(max)
+      #record_max(max, options[index])
+    end
+
+    def most_pressure
+      @max = -1
+      @max_path = nil
+      v = CavePath.new("AA", valves)
+
+      max_pressure(v)
+
+      return @max, @max_path
+    end
   end
 
   class Day16
     def self.run(argv)
-      puts "NOT YET"
-      #o = ProboscideaVolcanium.from(argv[0])
+      o = ProboscideaVolcanium.from(argv[0])
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      pressure, paths = o.most_pressure
+      ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+      puts "Part 1: #{pressure}. T. Elapsed: #{ending-starting} sec."
+
+      puts "Part 2: NOT YET"
     end
   end
 end
---
+++ day16_test.rb
@@ -0,0 +1,148 @@
+require 'minitest/autorun'
+require_relative 'day16'
+
+module Advent2022
+  class NameTest < Minitest::Test
+
+    def test_from
+      pv = ProboscideaVolcanium.from(sample)
+
+      assert_equal ["AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ" ], pv.graph.vertices
+      assert_equal 3, pv.graph.vertex("EE").flow_rate
+      assert_equal ["GG"], pv.graph.neighbours_names("HH")
+      assert_equal 2, pv.graph.neighbours("II").size
+      assert_equal 21, pv.graph.neighbours("II").last.flow_rate
+    end
+
+    def test_example_path_progress
+      pv = ProboscideaVolcanium.from(sample)
+      path = CavePath.new("AA", pv.valves)
+
+      assert_path(path, 0, "AA", ["AA"], 0)
+      assert_equal false, path.openable?
+      assert_equal 0, path.pressure_release
+
+      path.visit "DD"
+      assert_path(path, 1, "DD", ["AA", "DD"], 0)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 2, "DD", ["AA", "DD", "dd"], 0)
+      assert_equal 560, path.pressure_release
+
+      path.visit "CC"
+      assert_path(path, 3, "CC", ["AA", "DD", "dd", "CC"], 20)
+    
+      path.visit "BB"
+      assert_path(path, 4, "BB", ["AA", "DD", "dd", "CC", "BB"], 40)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 5, "BB", ["AA", "DD", "dd", "CC", "BB", "bb"], 60)
+
+      path.visit "AA"
+      assert_path(path, 6, "AA", ["AA", "DD", "dd", "CC", "BB", "bb", "AA"], 93)
+
+      path.visit "II"
+      assert_path(path, 7, "II", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II"], 126)
+
+      path.visit "JJ"
+      assert_path(path, 8, "JJ", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ"], 159)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 9, "JJ", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj"], 192)
+
+      path.visit "II"
+      assert_path(path, 10, "II", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II"], 246)
+
+      path.visit "AA"
+      assert_path(path, 11, "AA", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA"], 300)
+
+      path.visit "DD"
+      assert_path(path, 12, "DD", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD"], 354)
+
+      path.visit "EE"
+      assert_path(path, 13, "EE", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE"], 408)
+
+      path.visit "FF"
+      assert_path(path, 14, "FF", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF"], 462)
+
+      path.visit "GG"
+      assert_path(path, 15, "GG", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG"], 516)
+
+      path.visit "HH"
+      assert_path(path, 16, "HH", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH"], 570)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 17, "HH", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh"], 624)
+
+      path.visit "GG"
+      assert_path(path, 18, "GG", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG"], 700)
+
+      path.visit "FF"
+      assert_path(path, 19, "FF", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF"], 776)
+
+      path.visit "EE"
+      assert_path(path, 20, "EE", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF", "EE"], 852)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 21, "EE", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF", "EE", "ee"], 928)
+
+      path.visit "DD"
+      assert_path(path, 22, "DD", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF", "EE", "ee", "DD"], 1007)
+
+      path.visit "CC"
+      assert_path(path, 23, "CC", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF", "EE", "ee", "DD", "CC"], 1086)
+
+      res = path.open
+      assert_equal true, res
+      assert_path(path, 24, "CC", ["AA", "DD", "dd", "CC", "BB", "bb", "AA", "II", "JJ", "jj", "II", "AA", "DD", "EE", "FF", "GG", "HH", "hh", "GG", "FF", "EE", "ee", "DD", "CC", "cc"], 1165, true)
+
+      assert_equal 1246, path.pressure_release(25)
+      assert_equal 1327, path.pressure_release(26)
+      assert_equal 1408, path.pressure_release(27)
+      assert_equal 1489, path.pressure_release(28)
+      assert_equal 1570, path.pressure_release(29)
+      assert_equal 1651, path.pressure_release(30)
+    end
+
+    def test_most_pressure
+      pv = ProboscideaVolcanium.from(sample)
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      pressure, paths = pv.most_pressure
+      ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+      assert_equal 1651, pressure
+      puts "T. Elapsed: #{ending-starting} sec"
+    end
+
+    def assert_path(path, time, valve, visited, pressure, finished=false)
+      assert_equal time, path.time
+      assert_equal valve, path.valve
+      assert_equal visited, path.visited
+      assert_equal pressure, path.pressure_release(time)
+      assert_equal finished, path.finished?
+    end
+
+    def sample
+      sample = <<EOS
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+EOS
+
+      sample
+    end
+
+  end
+end
